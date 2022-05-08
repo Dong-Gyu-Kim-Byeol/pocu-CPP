1d84c49f215c32bcfdcfbed8fe220c83b3f3f639
LAP_4C8T\LAP <fwics88@gmail.com>
2022-05-08 23:46:41 +0900

    lab1

---
+++ Lab1/Add.cpp
@@ -0,0 +1,11 @@
+#include "Add.h"
+
+namespace lab1
+{
+
+    int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+}
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,11 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "Add.h"
+
 int main()
 {
+    assert(lab1::Add(1, 1) == 2);
 
+    return 0;
 }
